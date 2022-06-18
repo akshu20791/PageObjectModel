b7ec00f23b9b8b4bbd5115eec9085d1ce18e047f
akshu20791 <akshu20791@gmail.com>
2022-06-18 17:09:07 +0530

    updated test

--- src/main/java/com/crm/qa/util/TestUtil.java
+++ src/main/java/com/crm/qa/util/TestUtil.java
@@ -21,8 +21,8 @@ public class TestUtil extends TestBase {
 	public static long PAGE_LOAD_TIMEOUT = 20;
 	public static long IMPLICIT_WAIT = 20;
 
-	public static String TESTDATA_SHEET_PATH = "C:\\Users\\akshu\\workspace"
-			+ "//FreeCRMTest//src//main//java//com//crm//qa//testdata//FreeCrmTestData.xlsx";
+	public static String TESTDATA_SHEET_PATH = "C:\\Users\\akshu"
+			+ "\\PageObjectModel-master\\src\\main\\java\\com\\crm\\qa\\testdata\\FreeCrmTestData.xlsx";
 
 	static Workbook book;
 	static Sheet sheet;
